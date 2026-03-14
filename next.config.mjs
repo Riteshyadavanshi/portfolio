cea9db383769d7b78ab17bbd71358780540c7dfc
Riteshyadavanshi <riteshyadav4122@gmail.com>
2026-03-14 21:21:50 +0530

    sd

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     basePath: isProd ? '/portfolio' : '',
     assetPrefix: isProd ? '/portfolio/' : '',
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
